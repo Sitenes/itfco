077b2e2596fad7a2901de79d8e40ec0a19f2c5d5
MAAghakabiri <a.aghakabiri@gmail.com>
2024-12-04 16:13:04 +0330

    fix header

--- Toplearn/wwwroot/js/site.js
+++ Toplearn/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const headerLower = document.querySelector("#header");
 
-// Write your JavaScript code.
+    if (window.location.pathname === "/" || window.location.pathname.toLowerCase().includes("home")) {
+        headerLower.style.position = "absolute";
+    } else {
+        headerLower.style.position = "relative";
+    }
+});
